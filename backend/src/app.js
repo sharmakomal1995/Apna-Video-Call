63fe0778e03c14358987b3a07ad192d1dd883e74
komal sharma <komal03sharma@gmail.com>
2026-06-09 13:34:19 +0530

    project setup

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "Socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://komal03sharma:Sharma1995@cluster0.6tvcaip.mongodb.net/?appName=Cluster0");
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+}
+
+start();
