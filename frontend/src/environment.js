f8855b96192f0fa30f02e2be4dd50a87748da5b3
komal sharma <komal03sharma@gmail.com>
2026-06-11 14:53:23 +0530

    env

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apna-video-call-qzpd.onrender.com" :
+    "https://apnavideocallbackend-7882.onrender.com" :
     
     "http://localhost:8000";
     
